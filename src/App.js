bd714f84f940c9e29748df7db4313cafca45ee53
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-09-07 16:31:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1>Hello World test</h1>
+      <div>
+        <h1>Hello World test</h1>
+        <p>Hello test world</p>
+      </div>
     );
   }
 }
