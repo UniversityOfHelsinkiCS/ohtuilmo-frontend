08f7344b2a6fd57b25807f4a65165efdb9871cf8
Valtteri <valtsu54@hotmail.com>
2018-09-21 12:28:17 +0300

    added login form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  login = async (event) => {
+    event.preventDefault()
+    console.log("login")
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello World test!!</h1>
-        <p>Hello test world</p>
+        <h1>Login</h1>
+
+        <form onSubmit={this.login}>
+          <div>
+            name:
+          <input
+              name="name"
+            />
+          </div>
+          <div>
+            password:
+          <input
+              name="password"
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
       </div>
     );
   }
