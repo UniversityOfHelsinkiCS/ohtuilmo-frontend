21398c94e7b2590cf115e2e04d04c3284ff125bb
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-09-23 22:37:49 +0300

    axios added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import loginService from './services/login' 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
 
   login = async (event) => {
     event.preventDefault()
-    console.log("login")
+    await loginService.login({
+      username: this.state.username,
+      password: this.state.password
+    })
+  }
+
+  handleFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   render() {
     return (
       <div>
         <h1>Login</h1>
-
         <form onSubmit={this.login}>
           <div>
-            name:
-          <input
-              name="name"
+            <input
+              type='text'
+              name='username'
+              placeholder='username'
+              value={this.state.username}
+              onChange={this.handleFieldChange}
             />
           </div>
           <div>
-            password:
-          <input
-              name="password"
+            <input
+              type='password'
+              name='password'
+              placeholder='password'
+              value={this.state.password}
+              onChange={this.handleFieldChange}
             />
           </div>
-          <button type="submit">Login</button>
+          <button type='submit'>Login</button>
         </form>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/services/login.js
@@ -0,0 +1,14 @@
+// import axios from 'axios'
+
+// const baseUrl = "something"
+
+const login = async (credentials) => {
+    await console.log("logging in with: ", credentials)
+
+    // post credentials to backend, return response, also catch errors
+
+    // const response = await axios.post(`$baseUrl/login?`, credentials)
+    // return response.data
+}
+
+export default { login }
