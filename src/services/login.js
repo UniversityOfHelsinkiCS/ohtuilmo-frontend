115e173f368b15bdf937a3494331048a61f7dfbb
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-09-27 21:59:24 +0300

    local login test

--- src/services/login.js
+++ src/services/login.js
@@ -1,14 +1,13 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// const baseUrl = "something"
+const baseUrl = "http://localhost:3001/api/login"
 
 const login = async (credentials) => {
     await console.log("logging in with: ", credentials)
 
-    // post credentials to backend, return response, also catch errors
-
-    // const response = await axios.post(`$baseUrl/login?`, credentials)
-    // return response.data
+    // post credentials to backend, return response
+    const response = await axios.post(baseUrl, credentials)
+    return response.data
 }
 
 export default { login }
