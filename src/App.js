178c1b86719fa573119b1e24e4f8e02c80bf1101
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-10-08 11:15:13 +0300

    moved login function from app to loginpage component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import loginService from './services/login'
 import loginPageActions from './reducers/actions/loginPageActions'
 import LoginPage from './components/LoginPage'
 
 class App extends Component {
-  login = async (event) => {
-    event.preventDefault()
-    try {
-      const user = await loginService.login({
-        username: this.props.username,
-        password: this.props.password
-      })
-      console.log('success!!!', user)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
   render() {
     return (
       <Router>
         <div>
           <Route
-            exact
-            path="/"
+            exact path="/"
             render={() => (
               <LoginPage
                 username={this.props.username}
                 password={this.props.password}
                 updateUsername={this.props.updateUsername}
                 updatePassword={this.props.updatePassword}
-                login={this.login}
               />
             )}
           />
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,34 +1,50 @@
 import React from 'react'
+import loginService from '../services/login'
 
-const LoginPage = ({ username, password, updateUsername, updatePassword, login }) => (
-  <div>
-    <h1>Login</h1>
-    <form onSubmit={login}>
-      <div>
-        <input
-          type="text"
-          name="username"
-          placeholder="username"
-          value={username}
-          onChange={(e) => {
-            updateUsername(e.target.value)
-          }}
-        />
-      </div>
-      <div>
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => {
-            updatePassword(e.target.value)
-          }}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
-  </div>
-)
+const LoginPage = ({ username, password, updateUsername, updatePassword }) => {
+  const login = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username,
+        password
+      })
+      console.log('success!!!', user)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={login}>
+        <div>
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={username}
+            onChange={(e) => {
+              updateUsername(e.target.value)
+            }}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => {
+              updatePassword(e.target.value)
+            }}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
 
 export default LoginPage
