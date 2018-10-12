addf993cdacdc5f5456221bc15e48f6bcb40f65c
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2018-10-12 16:27:25 +0300

    Configured backend urls for staging and production environments

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,11 @@
 let BACKEND_URI = process.env.REACT_APP_BACKEND_URI
 
+if (window.location.hostname === 'svm-61.cs.helsinki.fi') {
+  BACKEND_URI = 'http://svm-61.cs.helsinki.fi:7001'
+} else if (window.location.hostname === 'svm-45.cs.helsinki.fi') {
+  BACKEND_URI = 'http://svm-45.cs.helsinki.fi:7001'
+}
+
 if (!BACKEND_URI) BACKEND_URI = 'http://localhost:7001'
 
 export { BACKEND_URI }
