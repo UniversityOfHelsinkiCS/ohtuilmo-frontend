5982cb3ff9be319bd5e3350fe2bba9237641d757
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2018-10-19 15:28:25 +0300

    Added styles and renamed stuff

--- src/components/common/ButtonAppBar.js
+++ src/components/common/ButtonAppBar.js
@@ -4,8 +4,6 @@ import { withStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
 
 const styles = {
   root: {
@@ -26,11 +24,8 @@ function ButtonAppBar(props) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" color="inherit" className={classes.grow}>
-            Ilmoittautumistyökalu
+            Ohjelmistotuotantoprojekti / Software engineering project
           </Typography>
         </Toolbar>
       </AppBar>
