93881859b0c6118caff9fb0dece49a32990269f9
responderi <rv.mahti@gmail.com>
2018-11-08 10:44:46 +0200

    Added dividers

--- src/components/TopicListPage.js
+++ src/components/TopicListPage.js
@@ -4,6 +4,9 @@ import topicListPageActions from '../reducers/actions/topicListPageActions'
 import List from '@material-ui/core/List'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItem from '@material-ui/core/ListItemText'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import Switch from '@material-ui/core/Switch'
+import Divider from '@material-ui/core/Divider'
 import topicService from '../services/topic'
 
 class TopicListPage extends React.Component {
@@ -16,6 +19,9 @@ class TopicListPage extends React.Component {
     console.log(this.props.topics)
   }
 
+  // ListItemText primary and secondary should be changed as {topic.content.title} and {topic.content.customerName}
+  // when the fetch is wanted to be done from the database (instead of topic.title and topic.customerName).
+
   render() {
     return(
       <div>
@@ -23,23 +29,35 @@ class TopicListPage extends React.Component {
           <List key={topic.id}>
             <a href="/">
               <ListItem>
-                <ListItemText primary={topic.content.title} secondary={topic.content.customerName} />
+                <ListItemText primary={topic.title} secondary={topic.customerName} />
+                <ListItemSecondaryAction>
+                  <Switch />
+                </ListItemSecondaryAction>
               </ListItem>
             </a>
+            <Divider inset />
           </List>
         ))}
         <List>
           <a href="/">
             <ListItem>
               <ListItemText primary='Static Topic Title' secondary='Static Customer Name' />
+              <ListItemSecondaryAction>
+                <Switch />
+              </ListItemSecondaryAction>
             </ListItem>
+            <Divider inset />
           </a>
         </List>
         <List>
           <a href="/">
             <ListItem>
               <ListItemText primary='Secondary Topic' secondary='Secondary Customer' />
+              <ListItemSecondaryAction>
+                <Switch />
+              </ListItemSecondaryAction>
             </ListItem>
+            <Divider inset />
           </a>
         </List>
       </div>
