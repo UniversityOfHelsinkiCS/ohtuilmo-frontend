c1533309b1f288a2d6f966e65677472718e51abf
ankatus <antti.j.kataja@gmail.com>
2018-11-23 16:20:22 +0200

    fixed a bunch of stuff

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,11 @@
 let BACKEND_URI = process.env.REACT_APP_BACKEND_URI
 
 if (window.location.hostname === 'svm-61.cs.helsinki.fi') {
-  BACKEND_URI = 'http://svm-61.cs.helsinki.fi:7001'
+  BACKEND_URI = 'http://svm-61.cs.helsinki.fi/projekti-backend'
 } else if (window.location.hostname === 'svm-45.cs.helsinki.fi') {
-  BACKEND_URI = 'http://svm-45.cs.helsinki.fi:7001'
+  BACKEND_URI = 'http://svm-45.cs.helsinki.fi/projekti-backend'
+} else if (window.location.hostname === 'studies.cs.helsinki.fi') {
+  BACKEND_URI = 'http://svm-45.cs.helsinki.fi/projekti-backend'
 }
 
 if (!BACKEND_URI) BACKEND_URI = 'http://localhost:7001'
