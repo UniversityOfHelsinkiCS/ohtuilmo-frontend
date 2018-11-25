9fcdc7869fec4ba106c609fbf8bdb8b8099c25e9
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2018-11-25 12:01:23 +0200

    Removed all but error console logs

--- src/services/login.js
+++ src/services/login.js
@@ -4,8 +4,6 @@ import { BACKEND_URI } from '../utils/config'
 const url = `${BACKEND_URI}/api/login`
 
 const login = async (credentials) => {
-  console.log('logging to', url)
-
   const response = await axios.post(url, credentials)
   return response.data
 }
