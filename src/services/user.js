e2f467edeb1897fca9201532a52964844a0df89b
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-11-29 13:28:27 +0200

    add service for updating user email, add general function for getting token

---
+++ src/services/user.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { BACKEND_URI } from '../utils/config'
+import { getUserToken } from '../utils/functions'
+
+const url = `${BACKEND_URI}/api/users`
+
+const update = async (user) => {
+  const config = {
+    headers: { 'Authorization': 'bearer ' + getUserToken() }
+  }
+  const response = await axios.post(url + `/${user.student_number}`, user.email, config)
+  return response.data
+}
+
+export default { update }
