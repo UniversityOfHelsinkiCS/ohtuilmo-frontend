48b7a6e9ca445a0ae778288c429e2731381c9a28
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2018-12-09 22:00:51 +0200

    Added admin check to admin page

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -3,6 +3,25 @@ import { connect } from 'react-redux'
 import './TopicFormPage.css'
 
 class AdminPage extends React.Component {
+  componentWillMount() {
+    try {
+      if (window.localStorage.getItem('loggedInUser') === null) {
+        window.location.replace(process.env.PUBLIC_URL + '/')
+      } else {
+        const token = JSON.parse(window.localStorage.getItem('loggedInUser'))
+        if (!token.user.admin || token === undefined || token === null) {
+          window.location.replace(process.env.PUBLIC_URL + '/')
+        }
+      }
+    } catch (e) {
+      console.log('error happened', e.response)
+      this.props.setError('Some error happened')
+      setTimeout(() => {
+        this.props.clearNotifications()
+      }, 3000)
+    }
+  }
+
   render() {
     return (
       <div className="admin-page-container">
