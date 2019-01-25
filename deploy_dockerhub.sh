2dba0ac0b4477d16400bef9aa87e911bcca7059b
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-01-25 12:12:30 +0200

    Fix missing chmod +x from deploy script

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -5,6 +5,15 @@
 #
 # DON'T RUN MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING! -Joona
 
+DOCKER_TAG_NAME=ohtuprojektiilmo/ohtufront
+
+echo 'Logging in with Docker'
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -t ohtuprojektiilmo/ohtufront .
-docker push ohtuprojektiilmo/ohtufront
+
+echo "Building $DOCKER_TAG_NAME"
+docker build -t $DOCKER_TAG_NAME .
+
+echo 'Pushing to dockerhub'
+docker push $DOCKER_TAG_NAME
+
+echo 'Deployed!'
