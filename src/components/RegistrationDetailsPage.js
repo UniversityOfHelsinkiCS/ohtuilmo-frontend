1fba53690475cf32d01c1ed56c312be8abfa3bf8
Aki Utoslahti <aki.utoslahti@helsinki.fi>
2019-02-05 16:25:54 +0200

    Finished registration details page
    Registration details page follows no same style as registration page.
    All details considering ones own registration are now show and along
    them the date of registration.
    
    Registration details page has been implemented in a fashion which makes
    it easy to later add feature like modifying ones own registration.

--- src/components/RegistrationDetailsPage.js
+++ src/components/RegistrationDetailsPage.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
+import ReactDragList from 'react-drag-list'
 
 import registrationActions from '../reducers/actions/registrationActions'
 
 import Typography from '@material-ui/core/Typography'
+import { Input, Card, CardContent, Select, MenuItem } from '@material-ui/core'
+import TopicDialog from './TopicDialog'
+
+import './RegistrationDetailsPage.css'
 
 class RegistrationDetailsPage extends React.Component {
   render() {
     const registration = this.props.ownRegistration
-    const { student, preferred_topics, questions } = registration
+    const { student, preferred_topics, questions, createdAt } = registration
     const { first_names, last_name, student_number, email } = student
 
     let firstname = ''
@@ -18,80 +23,76 @@ class RegistrationDetailsPage extends React.Component {
     }
     firstname = firstname.split(' ')[0]
 
-    const sortedQuestions = questions.sort((a, b) => {
-      if (a.type === 'scale' && b.type !== 'scale') {
-        return -1
-      } else if (a.type !== 'scale' && b.type === 'scale') {
-        return 1
-      }
-      return 0
-    })
-
-    const firstOpenTextQuestion = sortedQuestions.findIndex((question) => {
-      if (question.type === 'text') {
-        return true
-      }
-      return false
-    })
-
-    const scaleQuestions = sortedQuestions.slice(0, firstOpenTextQuestion)
-    const openTextquestions = sortedQuestions.slice(firstOpenTextQuestion)
+    const submittedDate = createdAt.replace(/T/, ' ').substr(0, 16)
 
     return (
       <div>
         <Typography variant="h4" gutterBottom>
           Registration details
         </Typography>
+        <Typography variant="body1" gutterBottom>
+          Registration date: {submittedDate}
+        </Typography>
         <div>
-          <Typography variant="h5" gutterBottom>
-            User details
-          </Typography>
+          <h2>User details</h2>
           <Typography variant="body1" gutterBottom>
             Name: {firstname} {last_name}
             <br />
             Student number: {student_number}
             <br />
             Email: {email}
-            <br />
           </Typography>
         </div>
         <div>
-          <Typography variant="h5" gutterBottom>
-            Preferred topics
-          </Typography>
-          {preferred_topics.map((topic, index) => {
-            const { title, customerName } = topic.content
-            return (
-              <Typography variant="body1" gutterBottom key={index}>
-                {index + 1}. {title} from {customerName}
-              </Typography>
-            )
-          })}
+          <h2>Preferred Topics</h2>
+          <div className="dragndrop-container">
+            <div className="dragndrop-indexes-container">
+              {preferred_topics.map((topic, index) => {
+                return (
+                  <Card key={index} className="dragndrop-index">
+                    {index + 1}
+                  </Card>
+                )
+              })}
+            </div>
+            <ReactDragList
+              className="dragndrop-list"
+              handles={false}
+              dataSource={preferred_topics}
+              rowKey="id"
+              row={(topic) => {
+                return <TopicDialog topic={topic} key={topic.content.title} />
+              }}
+              disabled
+            />
+          </div>
         </div>
         <div>
-          <Typography variant="h5" gutterBottom>
-            Answers to questions
-          </Typography>
-          <Typography variant="h6" gutterBottom>
-            Scale (0-5) questions
-          </Typography>
-          {scaleQuestions.map((question, index) => {
+          <h2>Details</h2>
+          {questions.map((question, index) => {
             return (
-              <Typography variant="body1" gutterBottom key={index}>
-                {question.question}: {question.answer}
-              </Typography>
-            )
-          })}
-          <Typography variant="h6" gutterBottom>
-            Open text questions
-          </Typography>
-          {openTextquestions.map((question, index) => {
-            return (
-              <Typography variant="body1" gutterBottom key={index}>
-                Question: {question.question}
-                <br />
-                Answer: {question.answer}
-              </Typography>
+              <Card style={{ marginBottom: '10px' }} key={index}>
+                <CardContent>
+                  <Typography variant="body1">{question.question}</Typography>
+                  {question.type === 'scale' ? (
+                    <Select value={question.answer} disabled>
+                      <MenuItem value={1}>1</MenuItem>
+                      <MenuItem value={2}>2</MenuItem>
+                      <MenuItem value={3}>3</MenuItem>
+                      <MenuItem value={4}>4</MenuItem>
+                      <MenuItem value={5}>5</MenuItem>
+                    </Select>
+                  ) : (
+                    <Input
+                      value={question.answer}
+                      fullWidth
+                      multiline
+                      rowsMax="3"
+                      disabled
+                    />
+                  )}
+                </CardContent>
+              </Card>
             )
           })}
         </div>
