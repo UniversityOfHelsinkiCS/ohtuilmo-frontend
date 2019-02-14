b3bb6f756476801c896b11d8e2cc790ded16543b
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-02-14 18:23:09 +0200

    Remove unnecessary build and install from e2e
    E2E script also now cleans up after itself by running `docker-compose down`.
    - It also runs faster now, as one unnecessary `npm run build` was removed, and
      the whole frontend app is only built once when building the Docker image.
    - Script now installs only Cypress according to
      package.json::devDependencies.cypress' version.
    - Travis now caches npm caches

--- run_cypress_e2e.sh
+++ run_cypress_e2e.sh
@@ -4,41 +4,103 @@ set -e
 
 dco='docker-compose -f docker-compose.e2e.yml'
 
-function install() {
-    # PREPARE IMAGES
-    echo 'Pulling and building images for E2E tests...'
-    $dco pull db backend nginx
-    # build frontend image from current directory, travis will build the latest
-    # version for testing
-    $dco build frontend
-    echo 'Images ready for E2E'
+function printProgress() {
+    local STAGE=$1
+    local TEXT=$2
+
+    local GREEN='\033[0;32m'
+    local NC='\033[0m' # No Color
+
+    echo -e "${GREEN}${STAGE}${NC}  ${TEXT}"
 }
 
-function run() {
+function prepareImages() {
+    # PREPARE IMAGES
+    printProgress 'INSTALL (0/2)' 'Pulling images for E2E tests...'
+    $dco pull db backend nginx
+
+    # build frontend image from current directory, travis will build the latest
+    # version for testing
+    printProgress 'INSTALL (1/2)' 'Pulled images, building frontend'
+    $dco build frontend
+
+    printProgress 'INSTALL (2/2)' 'Images ready for E2E'
+}
+
+function prepareServices() {
     # PREPARE SERVICES FOR TESTS
 
     # bring up db (1/3)
-    echo 'Preparing services for E2E tests...'
+    printProgress 'PREPARE (0/4)' 'Preparing services for E2E tests. Starting database...'
     $dco up -d db
-    # run migrations for backend (2/3)
+
+    printProgress 'PREPARE (1/4)' 'Database is up, running migrations'
     $dco run --entrypoint 'npm run db:migrate' --rm backend
 
-    # bring up frontend, backend and nginx (3/3)
+    printProgress 'PREPARE (2/4)' 'Migrations ran, starting backend, frontend and nginx'
     $dco up -d backend frontend nginx
 
-    # Services are now up and running, let's run cypress!
-    # don't use --production because we need cypress
-    echo 'Services prepared, installing and running...'
-    npm install
-    echo 'Installed! Running...'
-    CYPRESS_baseUrl=http://localhost:3000/projekti npm run test
+    printProgress 'PREPARE (3/4)' 'Infrastructure is ready, installing Cypress'
+    # install only Cypress, no need to spend time installing react-scripts
+    # will break if we require() some dependency in the integration tests!
+    local CYPRESS_VERSION=$(node -p "require('./package.json').devDependencies.cypress")
+    npm install --no-save "cypress@${CYPRESS_VERSION}"
+
+    printProgress 'PREPARE (4/4)' 'Cypress installed! Ready to run.'
+}
+
+function run() {
+    prepareServices
+
+    # don't exist if npm run test fails, we need to bring infra down afterwards
+    set +e
+    CYPRESS_baseUrl=http://localhost:3000/projekti npm run cypress:run
+    local TEST_RETURN_CODE=$?
+
+    $dco down
+    if [ "${TEST_RETURN_CODE}" != '0' ]; then
+        exit $TEST_RETURN_CODE
+    fi
+}
+
+function open() {
+    prepareServices
+
+    set +e
+    CYPRESS_baseUrl=http://localhost:3000/projekti npm run cypress:open
+    local TEST_RETURN_CODE=$?
+
+    $dco down
+    if [ "${TEST_RETURN_CODE}" != '0' ]; then
+        exit $TEST_RETURN_CODE
+    fi
 }
 
 if [ "$1" == 'run' ]; then
     run
-elif [ "$1" == 'install' ]; then
-    install
+elif [ "$1" == 'open' ]; then
+    open
+elif [ "$1" == 'prepare-images' ]; then
+    prepareImages
 else
-    echo "USAGE: $0 [run|install]"
+    echo "Run ohtuilmo-project's end-to-end (E2E) tests."
+    echo ''
+    echo 'USAGE:'
+    echo "   $0 <run|open|prepare-images>"
+    echo ''
+    echo '    The prepare-images command must be run before "run" or "watch".'
+    echo '    Port 3000 must be available for binding.'
+    echo ''
+    echo 'Commands:'
+    echo ''
+    echo '   run'
+    echo '      Runs the Cypress tests in CI mode.'
+    echo '   open'
+    echo '      Opens the Cypress GUI.'
+    echo '   prepare-images'
+    echo '      Pulls required images with docker-compose and'
+    echo '      builds the frontend service from the current'
+    echo '      directory.'
+    echo ''
     exit 1
 fi
