bd16dd8f69f2c0b5160ee413f9a4c7fa87277b2b
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-02-15 12:45:11 +0200

    Split e2e script into smaller jobs for .travis.yml
    By splitting the install, setup, run and teardown phases, we can apply them
    nicely to the travis lifecycle jobs.

--- run_cypress_e2e.sh
+++ run_cypress_e2e.sh
@@ -33,87 +33,88 @@ function printProgress() {
     echo -e "${GREEN}${STAGE}${NC}  ${TEXT}"
 }
 
-function prepareImages() {
+function install() {
     # PREPARE IMAGES
-    printProgress 'INSTALL (0/2)' 'Pulling images for E2E tests...'
+    printProgress 'INSTALL (0/3)' 'Pulling images for E2E tests...'
     $dco pull db backend nginx
 
     # build frontend image from current directory, travis will build the latest
     # version for testing
-    printProgress 'INSTALL (1/2)' 'Pulled images, building frontend'
+    printProgress 'INSTALL (1/3)' 'Pulled images, building frontend'
     docker build --pull --cache-from ohtuprojektiilmo/ohtufront --tag ohtuprojektiilmo/ohtufront .
 
-    printProgress 'INSTALL (2/2)' 'Images ready for E2E'
+    printProgress 'INSTALL (2/3)' 'Installing Cypress globally...'
+    npm i -g cypress@^3.1.5
+
+    printProgress 'INSTALL (3/3)' 'Cypress installed, installation complete.'
 }
 
-function prepareServices() {
-    # PREPARE SERVICES FOR TESTS
-
-    # bring up db (1/3)
-    printProgress 'PREPARE (0/3)' 'Preparing services for E2E tests. Starting database...'
+function setup() {
+    # Bring up services for tests
+    printProgress 'SETUP (0/3)' 'Preparing services for E2E tests. Starting database...'
     $dco up -d db
 
-    printProgress 'PREPARE (1/3)' 'Database is up, running migrations'
+    printProgress 'SETUP (1/3)' 'Database is up, running migrations'
     $dco run --entrypoint 'npm run db:migrate' --rm backend
 
-    printProgress 'PREPARE (2/3)' 'Migrations ran, starting backend, frontend and nginx'
+    printProgress 'SETUP (2/3)' 'Migrations ran, starting backend, frontend and nginx'
     $dco up -d backend frontend nginx
 
-    printProgress 'PREPARE (3/3)' 'Infrastructure is ready!'
+    printProgress 'SETUP (3/3)' 'Infrastructure is ready!'
+}
+
+function teardown() {
+    printProgress 'TEARDOWN (0/1)' 'Tearing down infra with docker-compose down'
+    $dco down
+    printProgress 'TEARDOWN (1/1)' 'Teardown complete!'
 }
 
 function run() {
-    prepareServices
-
-    # don't exist if npm run test fails, we need to bring infra down afterwards
-    set +e
     CYPRESS_baseUrl=http://localhost:3000/projekti npm run cypress:run
-    local TEST_RETURN_CODE=$?
-
-    $dco down
-    if [ "${TEST_RETURN_CODE}" != '0' ]; then
-        exit $TEST_RETURN_CODE
-    fi
 }
 
 function open() {
-    prepareServices
-
-    set +e
     CYPRESS_baseUrl=http://localhost:3000/projekti npm run cypress:open
-    local TEST_RETURN_CODE=$?
-
-    $dco down
-    if [ "${TEST_RETURN_CODE}" != '0' ]; then
-        exit $TEST_RETURN_CODE
-    fi
 }
 
 if [ "$1" == 'run' ]; then
     run
 elif [ "$1" == 'open' ]; then
     open
-elif [ "$1" == 'prepare-images' ]; then
-    prepareImages
+elif [ "$1" == 'install' ]; then
+    install
+elif [ "$1" == 'setup' ]; then
+    setup
+elif [ "$1" == 'teardown' ]; then
+    teardown
 else
     echo "Run ohtuilmo-project's end-to-end (E2E) tests."
     echo ''
     echo 'USAGE:'
-    echo "   $0 <run|open|prepare-images>"
+    echo "   $0 <run|open|install|setup|teardown>"
     echo ''
-    echo '    The prepare-images command must be run before "run" or "watch".'
+    echo '    "install" and "setup" must be run before "run" or "open".'
     echo '    Port 3000 must be available for binding.'
     echo ''
     echo 'Commands:'
     echo ''
     echo '   run'
     echo '      Runs the Cypress tests in CI mode.'
+    echo ''
     echo '   open'
     echo '      Opens the Cypress GUI.'
-    echo '   prepare-images'
+    echo ''
+    echo '   install'
     echo '      Pulls required images with docker-compose and'
     echo '      builds the frontend service from the current'
     echo '      directory.'
+    echo ''
+    echo '   setup'
+    echo '      Brings up the docker-compose network so that the tests can be'
+    echo '      run.'
+    echo ''
+    echo '   teardown'
+    echo '      Tears down the docker-compose network.'
     echo ''
     exit 1
 fi
