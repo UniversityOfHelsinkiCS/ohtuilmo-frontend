cb18b10d546604344d79c993f6b0e4a91368be52
PikkuHukka <olli.x.ojala@helsinki.fi>
2019-03-14 14:24:34 +0200

    Starting the deveploment of instructor view

---
+++ src/components/InstructorPage.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+
+//Services
+import groupManagementService from '../services/groupManagement'
+import peerReviewService from '../services/peerReview'
+
+const GroupsInstructed = ({ myGroups }) => {
+  return myGroups.map((myGroup, index) => {
+    return <GroupDetails key={index} myGroup={myGroup} />
+  })
+}
+
+const GroupDetails = ({ myGroup }) => {
+  if (!myGroup) {
+    return (
+      <div>
+        <h2>This user is currently not part of any group.</h2>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>{myGroup.groupName}</h2>
+        {myGroup.students.map((member, index) => {
+          return (
+            <p>
+              {index + 1}. {member.first_names} {member.last_name}
+            </p>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+/* const PeerReviewsGroups = ({ myGroups }) => {
+  return myGroups.map((myGroup, index) => {
+    return <GroupDetails key={index} myGroup={myGroup} />
+  })
+}
+ */
+class InstructorPage extends React.Component {
+  state = { myGroups: null, answersJson: null }
+
+  async componentDidMount() {
+    const myGroups = await groupManagementService.getByInstructor()
+    const peerReviewData = await peerReviewService.getAnswersByInstructor()
+
+    this.setState({
+      myGroups: myGroups,
+      answersJson: peerReviewData
+    })
+  }
+
+  render() {
+    const { myGroups, answersJson } = this.state
+    console.log('vastaukset json: ', answersJson)
+    console.log('group details renderin alussa ', myGroups)
+    if (myGroups) {
+      return (
+        <div className="instructor-container">
+          <h2>Ohjaat seuraavia ryhmiä: </h2>
+          <GroupsInstructed myGroups={myGroups} />
+          {/*           <PeerReviewsGroups myGroups={myGroups} />
+           */}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <p>Loading</p>
+        </div>
+      )
+    }
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    myGroups: state.myGroups,
+    answersJson: state.peerReviewData
+  }
+}
+
+const ConnectedInstructorPage = connect(mapStateToProps)(InstructorPage)
+
+// mapDispatchToProps
+
+export default withRouter(ConnectedInstructorPage)
