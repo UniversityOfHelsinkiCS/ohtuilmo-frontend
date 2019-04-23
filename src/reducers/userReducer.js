d2ce39ee23d62faf85547bb3751c15f25b29f6e5
Jere Helén <jere.helen@helsinki.fi>
2019-04-23 15:39:23 +0300

    Remove localStorage usage tracking script

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -4,8 +4,6 @@ const userReducer = (state = initialState, action) => {
   switch (action.type) {
   case 'LOGIN_USER':
     return action.payload
-  case 'LOGIN_TOKEN':
-    return action.payload
   case 'LOGOUT_USER':
     return initialState
   default:
