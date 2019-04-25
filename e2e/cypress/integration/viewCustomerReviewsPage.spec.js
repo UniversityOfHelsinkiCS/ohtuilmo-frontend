16cf48d6b7bccbe5e8d948dd1be06c4449247f51
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-04-25 17:32:35 +0300

    💯💯 SUPER HACK to access created groups etc

--- e2e/cypress/integration/viewCustomerReviewsPage.spec.js
+++ e2e/cypress/integration/viewCustomerReviewsPage.spec.js
@@ -36,9 +36,29 @@ const mockAnswers = [
   }
 ]
 
+const getAll = (...aliases) => {
+  const values = []
+  for (const alias of aliases) {
+    cy.get(alias).then((value) => values.push(value))
+  }
+  return cy.wrap(values)
+}
+
 describe('Customer review is shown', () => {
+  let locals = {}
+
   before(() => {
     cy.loginAsAdmin()
+
+    cy.createConfiguration({
+      name: 'Konfiguraatio A',
+      content: '',
+      registration_question_set_id: null,
+      review_question_set_1_id: null,
+      review_question_set_2_id: null,
+      customer_review_question_set_id: null
+    }).as('conf1')
+
     cy.createConfiguration({
       name: 'Conf 2',
       content: '',
@@ -46,108 +66,143 @@ describe('Customer review is shown', () => {
       review_question_set_1_id: null,
       review_question_set_2_id: null,
       customer_review_question_set_id: null
+    }).as('conf2')
+
+    cy.get('@conf1').then((configuration1) => {
+      cy.createTopic(
+        {
+          title: 'Konfiguraatio A Topic A',
+          customerName: 'Super nice customer',
+          email: 'asiakas@cee',
+          description: 'Decription..',
+          environment: 'Webbi',
+          specialRequests: '',
+          additionalInfo: ''
+        },
+        configuration1.id
+      ).as('conf1_topic1')
+
+      cy.get('@conf1_topic1').then((topic1) => {
+        cy.createGroup({
+          name: 'Group of Konfiguraatio A Topic A',
+          topicId: topic1.id,
+          configurationId: configuration1.id,
+          instructorId: null,
+          studentIds: ['012345678', '012345698']
+        }).as('conf1_topic1_group')
+      })
     })
-    cy.createTopic(
-      {
-        title: 'Conf 2 Topic C',
-        customerName: 'Super nice customer',
-        email: 'asiakas@cee',
-        description: 'Decription..',
-        environment: 'Webbi',
-        specialRequests: '',
-        additionalInfo: ''
-      },
-      2
-    )
-    cy.createTopic(
-      {
-        title: 'Conf 2 Topic D',
-        customerName: 'Super mad customer',
-        email: 'aasiakas@bee',
-        description: 'Decription..',
-        environment: 'Webbi',
-        specialRequests: '',
-        additionalInfo: ''
-      },
-      2
-    )
-    const id1 = cy.createGroupAndReturnId({
-      name: 'The group of the groups 1',
-      topicId: 1,
-      configurationId: 1,
-      instructorId: null,
-      studentIds: ['012345678', '012345698']
+
+    cy.get('@conf2').then((configuration2) => {
+      cy.createTopic(
+        {
+          title: 'Conf 2 Topic A',
+          customerName: 'Super nice customer',
+          email: 'asiakas@cee',
+          description: 'Decription..',
+          environment: 'Webbi',
+          specialRequests: '',
+          additionalInfo: ''
+        },
+        configuration2.id
+      ).as('conf2_topic1')
+
+      cy.createTopic(
+        {
+          title: 'Conf 2 Topic B',
+          customerName: 'Super mad customer',
+          email: 'aasiakas@bee',
+          description: 'Decription..',
+          environment: 'Webbi',
+          specialRequests: '',
+          additionalInfo: ''
+        },
+        configuration2.id
+      ).as('conf2_topic2')
+
+      getAll('@conf2_topic1', '@conf2_topic2').spread((topic1, topic2) => {
+        cy.createGroup({
+          name: 'Group of Conf 2 Topic A',
+          topicId: topic1.id,
+          configurationId: configuration2.id,
+          instructorId: null,
+          studentIds: ['012345678', '012345698']
+        }).as('conf2_topic1_group')
+
+        cy.createGroup({
+          name: 'Group of Conf 2 Topic B',
+          topicId: topic2.id,
+          configurationId: configuration2.id,
+          instructorId: null,
+          studentIds: ['012345678', '012345698']
+        }).as('conf2_topic2_group')
+      })
     })
-    console.log('GROUP ID', id1)
-    cy.createGroup({
-      name: 'The group of the groups 2',
-      topicId: 2,
-      configurationId: 1,
-      instructorId: null,
-      studentIds: ['012345678', '012345698']
+
+    // don't create review for @conf2_topic2_group
+    getAll('@conf1_topic1_group', '@conf2_topic1_group').each((group) => {
+      cy.createCustomerReview({
+        answer_sheet: mockAnswers,
+        group_id: group.id,
+        topic_id: group.topicId,
+        configuration_id: group.configurationId
+      })
     })
-    cy.createGroup({
-      name: 'The group of the groups 3',
-      topicId: 3,
-      configurationId: 2,
-      instructorId: null,
-      studentIds: ['012345678', '012345698']
-    })
-    cy.createGroup({
-      name: 'The group of the groups 4',
-      topicId: 4,
-      configurationId: 2,
-      instructorId: null,
-      studentIds: ['012345678', '012345698']
-    })
-    cy.createCustomerReview({
-      answer_sheet: mockAnswers,
-      group_id: id1,
-      topic_id: 1,
-      configuration_id: 1
-    })
-    cy.createCustomerReview({
-      answer_sheet: mockAnswers,
-      group_id: 2,
-      topic_id: 2,
-      configuration_id: 1
-    })
-    cy.createCustomerReview({
-      answer_sheet: mockAnswers,
-      group_id: 4,
-      topic_id: 5,
-      configuration_id: 2
-    })
+
+    const aliases = [
+      'conf1',
+      'conf2',
+      'conf1_topic1',
+      'conf1_topic1_group',
+      'conf2_topic1',
+      'conf2_topic2',
+      'conf2_topic1_group',
+      'conf2_topic2_group'
+    ]
+    for (const alias of aliases) {
+      cy.get('@' + alias).then((value) => {
+        locals[alias] = value
+      })
+    }
   })
+
   it('admin can visit customer reviews page', () => {
     cy.loginAsAdmin()
     cy.visit('/adminstration/customer-reviews')
     cy.contains('Customer reviews')
   })
-  it('all configurations are shwon', () => {
+
+  it('all configurations are shown', () => {
     cy.loginAsAdmin()
     cy.visit('/adminstration/customer-reviews')
     cy.contains('Customer reviews')
     cy.get('[data-cy="customer-reviews-select"]').click()
     cy.get('[data-cy="all-configurations"]').click()
-    cy.contains('The group of the groups 1')
-    cy.contains('The group of the groups 4')
+
+    const {
+      conf1_topic1_group,
+      conf2_topic1_group,
+      conf2_topic2_group
+    } = locals
+
+    cy.contains(conf1_topic1_group.name)
+    cy.contains(conf2_topic1_group.name)
+    cy.contains(conf2_topic2_group.name)
   })
+
   it('customer reviews for configuration 1 are shown', () => {
     cy.loginAsAdmin()
     cy.visit('/adminstration/customer-reviews')
     cy.get('[data-cy="customer-reviews-select"]').click()
-    cy.get('[data-cy="Konfiguraatio 1"]').click()
-    cy.contains('The group of the groups 1')
-    cy.contains('The group of the groups 2')
+
+    // todo
   })
+
   it('customer reviews for configuration 2 are shown', () => {
     cy.loginAsAdmin()
     cy.visit('/adminstration/customer-reviews')
     cy.get('[data-cy="customer-reviews-select"]').click()
-    cy.get('[data-cy="Conf 2"]').click()
-    cy.contains('No review submitted for group The group of the groups 3')
-    cy.contains('The group of the groups 4')
+
+    // todo
   })
-  after(() => {})
 })
