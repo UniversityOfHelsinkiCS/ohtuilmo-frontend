83825b27a2d0b2009ce94311f0fe5b537bd24583
Matti Luukkainen <mluukkai@iki.fi>
2019-08-15 21:56:40 +0300

    registration stats

--- src/components/Registrations.js
+++ src/components/Registrations.js
@@ -30,11 +30,86 @@ const Registrations = () => {
     )
   }
 
+  const stats = (registrations) => {
+
+    const votes = {}
+    const topicNames = {}
+
+    const TOPIC_COUNT = registrations[0].preferred_topics.length
+
+    registrations[0].preferred_topics.forEach(topic => {
+      topicNames[topic.id] = topic.content.title
+    })
+
+    registrations.forEach(reg => {
+      for (let i = 0; i < reg.preferred_topics.length; i++) {
+        const topic = reg.preferred_topics[i]
+        if (!votes[topic.id]) {
+          votes[topic.id] = []
+        }
+        votes[topic.id][i] = (votes[topic.id][i] || 0) + 1
+      }
+    })
+
+    const topics = Object.keys(votes).map(key => {
+      const topic = votes[key]
+
+      let totalScore = 0
+      let multiplyer = 1
+      for (let i = TOPIC_COUNT - 1; i >= 0; i--) {
+        if (topic[i]) {
+          totalScore += multiplyer * topic[i]
+        }
+        multiplyer *= TOPIC_COUNT + 1
+      }
+
+      return {
+        id: key,
+        name: topicNames[key],
+        score: totalScore,
+        votes: votes[key]
+      }
+    })
+
+    const byScore = (t1, t2) => t2.score - t1.score
+
+    const padded = (name) => {
+      const paddedName = name + '                                                      '
+      return paddedName.slice(0, 60)
+    }
+
+
+    const pretty = (votes) => {
+      const p = (score) =>
+        (score >= 10 ? score : ' ' + score)
+
+      let string = ''
+
+      for (let i = 0; i < 10; i++) {
+        string += p(votes[i] || 0) + ' '
+      }
+
+      return string
+    }
+
+    const lines = ['```\n']
+    topics.sort(byScore).forEach(topic => {
+      lines.push(`${padded(topic.name)} ${pretty(topic.votes)}\n`)
+    })
+    lines.push('```')
+
+    return lines
+  }
+
   return (
     <div>
       <h3>
         Registrations {regs.length}
       </h3>
+
+      <pre>
+        {stats(regs)}
+      </pre>
 
       <table>
         {regs.map(reg =>
