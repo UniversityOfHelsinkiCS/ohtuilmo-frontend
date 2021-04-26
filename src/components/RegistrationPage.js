cbae5100e1f47a3f043ae0e8ff92719f553c6f67
Matti Luukkainen <mluukkai@iki.fi>
2021-04-26 16:31:50 +0300

    tweak

--- src/components/RegistrationPage.js
+++ src/components/RegistrationPage.js
@@ -236,7 +236,7 @@ class RegistrationPage extends React.Component {
               }}
             >
               Huomaa, että projektiin osallistuminen edellyttää että kaikki esitietona olevat opintojaksot eli kurssi Ohjelmistotuotanto sekä kaksi aineopintojen harjoitustyötä
-              (tai avoimen yliopiston opiskelijoilla Full stack -websovelluskehitys) on suoritettu projektin alkuun mennessä.
+              (tai avoimen yliopiston opiskelijoilla Full stack -websovelluskehitys) on tehtynä (suoritettu tai palautettu arvosteltavaksi) projektin alkuun mennessä.
             </div>
             <h2 className="landingpage-header">User details</h2>
             <UserDetails />
